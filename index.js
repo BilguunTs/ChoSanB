dd10c3c2d739ea4cc11998f7834508d458a9497b
T0T0R0-LAP\User <usosketch@yahoo.com>
2021-12-22 22:44:15 +0800

    dd

--- index.js
+++ index.js
@@ -5,5 +5,5 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-
+//so this is first comment
 AppRegistry.registerComponent(appName, () => App);
